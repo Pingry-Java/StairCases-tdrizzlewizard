7cac4717bbc4f83455464d5393a3fe9b2f06d491
Tom Drzik <tomdrzik@gmail.com>
2016-09-25 18:04:15 -0400

    Doesn't work but has for loop may come back to later

--- StairCases.java
+++ StairCases.java
@@ -1,7 +1,10 @@
 /**
- * Write javadoc for your class here (and remove this comment, or course)
- *
+ * This class takes a user-inputed number and prints out a staircases, one asceding to the specified number and one descending away from it
+ *@author Tom Drzik
+ *@version 0.1
  */
+
+import java.util.Scanner;
 
 public class StairCases
 {
@@ -12,22 +15,36 @@ public class StairCases
 
   public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
-
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Type the number of rows you want");
+    int size = keyboard.nextInt();
     printAscending(size);
     printDescending(size);
   }
 
   /**
-   * Write your javadoc here
+   * This method will print rows of Xs until it reaches the specified number
    */
-  public static void printAscending
-  //TODO Finish this method
+  public static void printAscending(int size)
+  {
+      for (int x = 1; x <= size; x++;)
+          System.out.print("x");
+      
+      System.out.println();
+      
+      printAscending(size);
+    
+        
+  }
 
-  /**
+  
+      
+/**
    * Write your javadoc here
    */
-  public static void printDescending
+  public static void printDescending(int size)
+  {
+      
+  }
   //TODO Finish this method
 }
